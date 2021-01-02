93a2759d8c2d1e5952eb7d23507f827b3c5ce131
MarPjCN <pjajcik@cngroup.dk>
2021-01-02 15:51:20 +0100

    Initial Commit

---
+++ mt_project/mt_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  mt_projectApp.swift
+//  mt_project
+//
+//  Created by Pjajčík Marián on 02.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct mt_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
